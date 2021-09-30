281298353eda68444a90174c3ed52acc9a567170
jrryoo <jryoo@psu.edu>
2021-09-30 15:28:31 -0400

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+def make_blink(function):
+	"""Defines the decorator"""
+
+	#This makes the decorator transparent in terms of its name and docstring
+
+
+	#Define the inner function
+	def decorator():
+		#Grab the return value of the function being decorated
+		
+
+		#Add new functionality to the function being decorated
+		
+
+	return decorator
+
+#Apply the decorator here!
+
+def hello_world():
+	"""Original function! """
+
+	return "Hello, World!"
+
+#Check the result of decorating
+
+
+#Check if the function name is still the same name of the function being decorated
+
+
+#Check if the docstring is still the same as that of the function being decorated
+
+
