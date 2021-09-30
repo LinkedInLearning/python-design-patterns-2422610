603c91c13e138819fc30e811833b28652b373d87
jrryoo <jryoo@psu.edu>
2021-09-30 15:38:08 -0400

    Add files via upload

---
+++ iterator.py
@@ -0,0 +1,20 @@
+def count_to(count):
+	"""Our iterator implementation"""
+	
+	#Our list
+	numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
+
+	#Our built-in iterator
+	#Creates a tuple such as (1, "eins")
+	
+	
+	#Iterate through our iterable list
+	#Extract the German numbers
+	#Put them in a generator called number
+	for position, number in iterator:
+		
+		#Returns a 'generator' containing numbers in German
+		
+#Let's test the generator returned by our iterator
+
+	
